2059a81e92913fd58b9c421e4ee95468af101d11
christian barbosa <christian.barbosa@ewally.com.br>
2025-04-20 18:01:12 -0300

    wip test deploy

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+import cloudflare from "@astrojs/cloudflare";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), tailwind()],
+  experimental: {
+    svg: true,
+  },
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  })
+});
